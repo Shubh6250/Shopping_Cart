0670ccd355a7d2eb9e73c2a7c4181fcb6d490700
Shubham <deshpande460@gmail.com>
2022-05-23 11:52:20 +0530

    my first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Products from '../Components/Products'
+
+
+const Home = () => {
+  return (
+      <>
+       <h2 className='heading'>Welcome to the rdux toolkit store</h2>
+    <section>
+    <h3>Products</h3>
+    <Products/>
+
+    </section>
+
+
+      </>
+   
+  )
+}
+
+export default Home
